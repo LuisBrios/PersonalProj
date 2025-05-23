3e0d4a58b7b71febdaa0761a30b1a3b36226ba89
Luis Barrios <130001003+LuisBrios@users.noreply.github.com>
2025-05-22 23:39:53 -0700

    Update test.cpp
    Added "flood-fill" logic to make sure that bunches of zeroes will be marked, making it easier for the player.

--- test.cpp
+++ test.cpp
@@ -10,6 +10,7 @@ int xMax = 0;
 int yMax = 0; 
 vector<vector<char>> mineBoard; 
 vector<vector<char>> playerBoard; 
+vector<vector<bool>> truthBoard; 
 
 void promptGame() {
     cout << "Choose your difficulty" << endl;
@@ -25,20 +26,19 @@ void boardCreate (int x, int y) {
     for (int i = 0; i < y; ++i){
         for (int j = 0; j < x; ++j){
             //replace with playerboard later.
-            cout << "|_" << mineBoard[i][j] << "_|";
+            cout << "|_" << playerBoard[i][j] << "_|";
         } 
         cout << endl; 
     }
 }
 
-//check mines near with moves. 
-int checkNearbyMines(int y, int x){
+int checkNearbyMines(int x, int y){
     int numMines = 0; 
     for (int i = 0; i < 8; ++i) {
-        if ((movesX[i] + x > xMax) || (movesY[i] + y > yMax) || (movesX[i] + x < 0) || (movesY[i] + y < 0)){
+        if ((movesX[i] + x >= xMax) || (movesY[i] + y >= yMax) || (movesX[i] + x < 0) || (movesY[i] + y < 0)){
             continue;
         } else {
-            if (mineBoard[y][x] == '*'){
+            if (mineBoard[movesY[i] + y][movesX[i] + x] == '*'){
                 ++numMines; 
             }
         }
@@ -46,13 +46,39 @@ int checkNearbyMines(int y, int x){
     return numMines; 
 }
 
+void openZeros(int x, int y){
+    int val = 0; 
+    //out of bounds check
+    if ((x < 0) || (x >= xMax) || (y < 0) || (y >= yMax) || (truthBoard[y][x] == true)){
+        return; 
+    }
+    val = checkNearbyMines(x, y); 
+    if (val != 0){
+        playerBoard[y][x] = char(val + 48);
+        truthBoard[y][x] = true; 
+        return; 
+    } else if (val == 0){
+        playerBoard[y][x] = '0';
+        truthBoard[y][x] = true; 
+    } 
+    //recursive for all zeros 
+    openZeros(x + 1, y); 
+    openZeros(x - 1, y); 
+    openZeros(x, y + 1); 
+    openZeros(x, y - 1); 
+}
+
 bool mineChecker (int x, int y) {
     int mineVal = 0; 
     if (mineBoard[y][x] == '*'){
+        playerBoard[y][x] = '*'; 
         return true; 
     } else {
-        mineVal = checkNearbyMines(x, y);
-        mineBoard[y][x] = mineVal; 
+        mineVal = (checkNearbyMines(x, y) + 48); 
+        playerBoard[y][x] = char(mineVal);
+            if (playerBoard[y][x] == '0'){
+                openZeros(x, y); 
+            }
         return false; 
     }
 }
@@ -74,6 +100,7 @@ void mineSetter(int x, int y, int n){
 void mineInitiation(int lvl) {
     int dimensionX = 0, dimensionY = 0, numMines = 0;
     //1 = Easy, 2 = Medium, 3 = Hard, 4 = Random Mines
+    //still have yet to implement lvl 4 lol 
     if (lvl == 1){
         dimensionX = 5, dimensionY = 5, numMines = 10; 
     } else if (lvl == 2){
@@ -85,6 +112,8 @@ void mineInitiation(int lvl) {
     xMax = dimensionX; 
     mineBoard.resize(dimensionY, vector<char>(dimensionX, '-'));
     playerBoard.resize(dimensionY, vector<char>(dimensionX, '-'));
+    truthBoard.resize(dimensionY, vector<bool>(dimensionX, false));
+
     mineSetter(dimensionX, dimensionY, numMines);
 }
 
@@ -103,10 +132,9 @@ int main() {
     } 
     mineInitiation(lvl);
     while (detonated == false){
-        //Soon, account for invalidnumbers being selected. So basically make board coords global soon lol.
         cout << "Select a coordinate: " << endl; 
         cin >> xSpot >> ySpot; 
-        detonated = mineChecker(ySpot, xSpot);
+        detonated = mineChecker(xSpot, ySpot);
         boardCreate(xMax, yMax); 
     }
     return 0;
